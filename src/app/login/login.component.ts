f5166ceedf79ceb60b6f1fca7df033017961b746
yenaung <yenaung1921992@gmail.com>
2024-06-11 23:55:26 +0800

    added form control in login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      userEmail: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
 }
